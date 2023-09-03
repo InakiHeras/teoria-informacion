126705ac7c0386d4f6bc05844eab46a4caf10294
Iñaki Heras <ihgongora2001@gmail.com>
2023-09-02 23:07:56 -0500

    Modificación considerables. Se agrego la fuente de información: video, se modificaron las clases para recibir frame por frame.

--- scheme.py
+++ scheme.py
@@ -1,13 +1,22 @@
 import random
 import time
-import queue
-import threading
+import cv2
 
-# Fuente de Información
+# Fuente de Información -> VIDEO
 class FuenteInformacion:
+    def __init__(self, video):
+        self.video = cv2.VideoCapture(video)
+        self.fps = int(self.video.get(cv2.CAP_PROP_FPS))
+        self.ancho = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.altura = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
     def generar_datos(self):
-        # Genera datos de la fuente de información (en este caso, números aleatorios) (MODIFICAR)
-        return [random.randint(0, 255) for _ in range(10)]  # Ejemplo de 10 números entre 0 y 255
+        # Leer frame por frame
+        ret, frame = self.video.read()
+        if not ret:
+            # Si llegamos al final del video, regresa un None
+            return None
+        return frame 
 
 # Transmisor -> FALTA CONVERTIR A BINARIO
 class Transmisor:
@@ -16,11 +25,13 @@ class Transmisor:
         self.canal = canal
 
     def transmitir(self):
-        datos = self.fuente.generar_datos()
-        for dato in datos:
-            paquete = PaqueteUDP(dato)
-            self.canal.enviar(paquete)
-
+        while True:
+            frame = self.fuente.generar_datos()
+            if frame is None:
+                break
+            #paquete = PaqueteVideo(frame)
+            self.canal.enviar(frame)
+            
 # Canal de Comunicación
 class CanalComunicacion:
     def __init__(self, velocidad):
@@ -28,7 +39,7 @@ class CanalComunicacion:
 
     def enviar(self, paquete):
         # Simula el canal de comunicación con una probabilidad de pérdida de paquetes
-        if random.random() < 0.3:  # Probabilidad de pérdida del 30%
+        if random.random() < 0.5:  # Probabilidad de pérdida del 30%
             print("Paquete perdido:", paquete)
         else:
             # Simula la velocidad de 802.11n
@@ -36,36 +47,35 @@ class CanalComunicacion:
             self.retransmitir(paquete)
 
     def retransmitir(self, paquete):
-        print("Paquete enviado:", paquete)
+        #print("Paquete enviado:", paquete)
         # Simula la retransmisión del paquete al receptor
         receptor.recibir(paquete)
 
 # Receptor
 class Receptor:
+    def __init__(self, fps, ancho, altura):
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.video_salida = cv2.VideoWriter('one_piece_recibido.mp4', fourcc, fps, (ancho, altura))
+
     def recibir(self, paquete):
-        # Simula la recepción del paquete por el receptor
-        print("Paquete recibido por el receptor:", paquete)
-        # Procesar el paquete, aquí puedes implementar la lógica necesaria
+        frame = paquete
+        self.video_salida.write(frame)
+    
+    def cerrar_video(self):
+        self.video_salida.release()
 
-# Clase que representa un paquete UDP con encabezado y cola
-class PaqueteUDP:
-    def __init__(self, dato):
-        self.encabezado = "UDP_HEADER"
-        self.dato = dato
-        self.cola = "UDP_TAIL"
-
-    def __str__(self):
-        return f"{self.encabezado} {self.dato} {self.cola}"
-
+'''
+class PaqueteVideo:
+    def __init__(self, frame):
+        self.frame = frame
+'''
 # Función principal
 if __name__ == "__main__":
     # Crear objetos de las clases
-    fuente = FuenteInformacion()
-    canal = CanalComunicacion(0.1)  # Velocidad de 802.11n simulada (0.1 segundos) (MODIFICAR)
-    receptor = Receptor()
-
-    # Crear y ejecutar hilos para el transmisor
+    fuente = FuenteInformacion('one_piece.mp4')
+    canal = CanalComunicacion(0.01)  # Velocidad de 802.11n simulada (0.1 segundos) (MODIFICAR)
+    receptor = Receptor(fuente.fps, fuente.ancho, fuente.altura)
     transmisor = Transmisor(fuente, canal)
-    transmisor_thread = threading.Thread(target=transmisor.transmitir)
-    transmisor_thread.start()
-    transmisor_thread.join()
+
+    transmisor.transmitir()
+    receptor.cerrar_video()
