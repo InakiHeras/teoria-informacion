d10040304cdaa60ddd28dc420779269427878cef
Iñaki Heras <ihgongora2001@gmail.com>
2023-09-17 14:13:59 -0500

    Se agregó la codificación a binario y el calculo de la entropia

--- scheme.py
+++ scheme.py
@@ -1,6 +1,9 @@
 import random
 import time
 import cv2
+import struct
+import numpy as np
+import math
 
 # Fuente de Información -> VIDEO
 class FuenteInformacion:
@@ -18,7 +21,7 @@ class FuenteInformacion:
             return None
         return frame 
 
-# Transmisor -> FALTA CONVERTIR A BINARIO
+# Transmisor
 class Transmisor:
     def __init__(self, fuente, canal):
         self.fuente = fuente
@@ -29,18 +32,22 @@ class Transmisor:
             frame = self.fuente.generar_datos()
             if frame is None:
                 break
-            #paquete = PaqueteVideo(frame)
-            self.canal.enviar(frame)
+            paquete = struct.pack(f'{frame.size}s', frame.tobytes())
+            self.canal.enviar(paquete)
             
 # Canal de Comunicación
 class CanalComunicacion:
     def __init__(self, velocidad):
         self.velocidad = velocidad
+        self.ruido_list = []
 
     def enviar(self, paquete):
         # Simula el canal de comunicación con una probabilidad de pérdida de paquetes
-        if random.random() < 0.5:  # Probabilidad de pérdida del 30%
-            print("Paquete perdido:", paquete)
+        if random.random() < 0.3:  # Probabilidad de pérdida
+            #print("Paquete corrupto")
+            paquete_corrupto = self.introducir_ruido(paquete)
+            self.retransmitir(paquete_corrupto)
+            self.ruido_list.append(random.random())
         else:
             # Simula la velocidad de 802.11n
             time.sleep(self.velocidad)
@@ -51,6 +58,21 @@ class CanalComunicacion:
         # Simula la retransmisión del paquete al receptor
         receptor.recibir(paquete)
 
+    def introducir_ruido(self, paquete):
+        # Cambiar aleatoriamente algunos bytes del paquete
+        paquete_corrupto = bytearray(paquete)
+        num_errores = random.randint(1, int(0.4 * len(paquete))) # Simular hasta el 40% de errores
+        for _ in range(num_errores):
+            indice = random.randint(0, len(paquete_corrupto) - 1)
+            paquete_corrupto[indice] = random.randint(0, 255) # Cambiar a un valor aleatorio de 0 a 255
+        return bytes(paquete_corrupto)
+    
+    def calcular_entropia(self):
+        probabilidades = [num / len(self.ruido_list) if num < 0.3 else 0 for num in self.ruido_list]
+        entropia = -sum(p * math.log2(p) for p in probabilidades if p > 0)
+        return entropia
+
+
 # Receptor
 class Receptor:
     def __init__(self, fps, ancho, altura):
@@ -58,7 +80,8 @@ class Receptor:
         self.video_salida = cv2.VideoWriter('one_piece_recibido.mp4', fourcc, fps, (ancho, altura))
 
     def recibir(self, paquete):
-        frame = paquete
+        frame = np.frombuffer(struct.unpack(f'{len(paquete)}s', paquete)[0], dtype=np.uint8)
+        frame = frame.reshape((fuente.altura, fuente.ancho, 3))
         self.video_salida.write(frame)
     
     def cerrar_video(self):
@@ -73,9 +96,12 @@ class PaqueteVideo:
 if __name__ == "__main__":
     # Crear objetos de las clases
     fuente = FuenteInformacion('one_piece.mp4')
-    canal = CanalComunicacion(0.01)  # Velocidad de 802.11n simulada (0.1 segundos) (MODIFICAR)
+    canal = CanalComunicacion(0.01)  # Velocidad de 802.11n simulada (0.1 segundos)
     receptor = Receptor(fuente.fps, fuente.ancho, fuente.altura)
     transmisor = Transmisor(fuente, canal)
 
     transmisor.transmitir()
     receptor.cerrar_video()
+
+    entropia = canal.calcular_entropia()
+    print(entropia)
